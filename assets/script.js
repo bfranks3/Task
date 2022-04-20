f77f34c34c0da721ed3f2551c71c7429a1565cbe
Brandon <captainxcuddles@hotmail.com>
2022-04-19 19:37:34 -0600

    adding the option and formEl to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
 
     var listItemEl = document.createElement("li");
 
@@ -11,6 +13,8 @@ var createTaskHandler = function() {
     listItemEl.textContent = "This is a new task.";
     
     tasksToDoEl.appendChild(listItemEl);
+
+    console.log(event);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
